900caf340cac481a01c928b3c7b808cd730d34c7
pat-alt <altmeyerpat@gmail.com>
2025-02-18 12:37:21 +0100

    using csl file

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## Version [0.0.2]
+
+Changes in this version address this discussion on GitHub: [open the discussion](https://github.com/quarto-dev/quarto-cli/discussions/12101).
+
+### Changed
+
+- As suggested by @cderv, the template does indeed not support `natbib`. 
---
+++ _extensions/quarto-ecml/cite.lua
@@ -0,0 +1,24 @@
+return {
+    Cite = function(el)
+      if quarto.doc.is_format("latex") then
+        -- Check if any citation ID does NOT contain a dash
+        -- (indicating it's likely a bibliographic citation)
+        local isCitation = false
+        for _, cite in ipairs(el.citations) do
+          if not cite.id:match("-") then
+            isCitation = true
+            break
+          end
+        end
+        
+        if isCitation then
+          local cites = el.citations:map(function(cite)
+            return cite.id
+          end)
+          local citesStr = "\\cite{" .. table.concat(cites, ",") .. "}"
+          return pandoc.RawInline("latex", citesStr)
+        end
+      end
+      return el
+    end
+}
