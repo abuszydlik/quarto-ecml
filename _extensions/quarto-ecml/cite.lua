c29ffa58f191bdc68841f9daecbc8cdcfddac237
pat-alt <altmeyerpat@gmail.com>
2025-03-05 20:51:24 +0100

    minor modifications to make it work locally. Attempt to apply filter conditionally on metadata but no luck.

--- _extensions/quarto-ecml/cite.lua
+++ _extensions/quarto-ecml/cite.lua
@@ -1,24 +1,27 @@
-return {
-    Cite = function(el)
-      if quarto.doc.is_format("latex") then
-        -- Check if any citation ID does NOT contain a dash
-        -- (indicating it's likely a bibliographic citation)
-        local isCitation = false
-        for _, cite in ipairs(el.citations) do
-          if not cite.id:match("-") then
-            isCitation = true
-            break
-          end
-        end
-        
-        if isCitation then
-          local cites = el.citations:map(function(cite)
-            return cite.id
-          end)
-          local citesStr = "\\cite{" .. table.concat(cites, ",") .. "}"
-          return pandoc.RawInline("latex", citesStr)
-        end
+Cite = function(el)
+  -- Check if the document is being rendered to LaTeX
+  if quarto.doc.is_format("latex") then
+    -- Check if any citation ID does NOT contain a dash
+    -- (indicating it's likely a bibliographic citation)
+    local isCitation = false
+    for _, cite in ipairs(el.citations) do
+      if not cite.id:match("-") then
+        isCitation = true
+        break
       end
-      return el
     end
+    
+    if isCitation then
+      local cites = el.citations:map(function(cite)
+        return cite.id
+      end)
+      local citesStr = "\\cite{" .. table.concat(cites, ",") .. "}"
+      return pandoc.RawInline("latex", citesStr)
+    end
+  end
+  return el
+end
+
+return {
+  Cite=Cite
 }
